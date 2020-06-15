771d1e30148418695e30a5ba7ee1a6308f97fb8e
Yung, Chun Kau <yung.chun.kau@gmail.com>
2020-06-15 09:06:03 +0800

    03.06 a. Simple question - print a blank line per 5 output lines and etc
    b. First IEnumerable<T> func()
    c. LINQ Zip() - Print a blank line per 5 output lines
    d. A simple question

--- Program.cs
+++ Program.cs
@@ -36,6 +36,7 @@ namespace dir2
                 IsIncludingFilename = (it) => !it.EndsWith(exclFileExtension);
             }
 
+            var countWriteLine = 0;
             var count = Helper.GetAllFiles(baseDir)
                 .Where((filename) => IsIncludingFilename(filename))
                 .Select((filename) => new FileInfo(filename))
@@ -44,6 +45,12 @@ namespace dir2
                     Console.Write($"{info.Length,7} ");
                     Console.Write($"{info.LastWriteTime:yyyy-MM-dd HH:mm:ss} ");
                     Console.WriteLine(info.FullName);
+                    countWriteLine += 1;
+                    if (countWriteLine >= 5)
+                    {
+                        countWriteLine = 0;
+                        Console.WriteLine();
+                    }
                     return info;
                 })
                 .Count();
--- readme.txt
+++ readme.txt
@@ -1,1 +1,5 @@
-03.05 c. Replace control data 'exclFileExtension' by a lambda
+03.06 a. Simple question - print a blank line per 5 output lines and etc
+
+b. First IEnumerable<T> func()
+c. LINQ Zip() - Print a blank line per 5 output lines
+d. A simple question

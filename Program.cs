16b8d5165974dff652f4c3ef79428dc44014b9a2
Yung, Chun Kau <yung.chun.kau@gmail.com>
2020-06-15 09:21:46 +0800

    05.01 Add 'MyFilter()' for the feature of LINQ Where()

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace dir2
@@ -34,19 +35,31 @@ namespace dir2
                 IsIncludingFilename = (it) => !it.EndsWith(exclFileExtension);
             }
 
+            var filterResult = MyFilter(
+                Helper.GetAllFiles(baseDir),
+                filterThe: (it) => IsIncludingFilename(it));
+
             var count = 0;
-            foreach (var filename in Helper.GetAllFiles(baseDir))
+            foreach (var filename in filterResult)
             {
-                if (!IsIncludingFilename(filename))
-                {
-                    continue;
-                }
                 Console.WriteLine(filename);
                 count += 1;
             }
             Console.WriteLine($"{count} files are found.");
 
             return;
+        }
+
+        static IEnumerable<string> MyFilter(
+            IEnumerable<string> seqThe, Func<string, bool> filterThe)
+        {
+            var enumThe = seqThe.GetEnumerator();
+            while (enumThe.MoveNext())
+            {
+                var currentThe = enumThe.Current;
+                if (!filterThe(currentThe)) continue;
+                yield return currentThe;
+            }
         }
     }
 }
--- readme.txt
+++ readme.txt
@@ -1,3 +1,1 @@
-05.00 Lesson Init
-
-Get Program.cs from 01.02 and lambda filter '--excl-ext=' from "03.05c"
+05.01 Add 'MyFilter()' for the feature of LINQ Where()

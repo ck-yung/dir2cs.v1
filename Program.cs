8233045b3a6a360a6900420a1613f6e7ce83ee2e
Yung, Chun Kau <yung.chun.kau@gmail.com>
2020-06-15 09:21:11 +0800

    05.00 Lesson Init
    Get Program.cs from 01.02 and lambda filter '--excl-ext=' from "03.05c"

--- Program.cs
+++ Program.cs
@@ -25,10 +25,26 @@ namespace dir2
             var baseDir = Directory.GetCurrentDirectory();
             Console.WriteLine($"{baseDir}:");
 
+            Func<string, bool> IsIncludingFilename = (_) => true;
+            var exclFileExtensionOpt = "--excl-ext=";
+            foreach (var arg in args)
+            {
+                if (!arg.StartsWith(exclFileExtensionOpt)) continue;
+                var exclFileExtension = arg.Substring(exclFileExtensionOpt.Length);
+                IsIncludingFilename = (it) => !it.EndsWith(exclFileExtension);
+            }
+
+            var count = 0;
             foreach (var filename in Helper.GetAllFiles(baseDir))
             {
+                if (!IsIncludingFilename(filename))
+                {
+                    continue;
+                }
                 Console.WriteLine(filename);
+                count += 1;
             }
+            Console.WriteLine($"{count} files are found.");
 
             return;
         }
--- readme.txt
+++ readme.txt
@@ -1,1 +1,3 @@
-04.07 b. Exception handling for MoveNext and Current
+05.00 Lesson Init
+
+Get Program.cs from 01.02 and lambda filter '--excl-ext=' from "03.05c"
